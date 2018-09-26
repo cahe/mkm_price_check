5ab85f7e768739e0204c742810f39b607671b4e7
Maciej Janowski <maciej.janowski@gmail.com>
2018-09-27 00:33:13 +0200

    Fix table definitions

--- setup_sqlite.php
+++ setup_sqlite.php
@@ -2,5 +2,24 @@
 require_once 'config.php';
 
 $db = new SQLite3($config['dbFile']);
-$db->query("CREATE TABLE expansions (id INT, code VARCHAR(10), name VARCHAR(255))");
-$db->query("CREATE TABLE stock (id INT, name VARCHAR(255), low DECIMAL(10,2), stock INTEGER, expansion_code varchar(10), avg DECIMAL(10,2), my_price DECIMAL(10,2), cheapest_in_country DECIMAL(10,2))");
+$db->query("
+CREATE TABLE `expansions` (
+	`id`	INT,
+	`code`	VARCHAR ( 10 ),
+	`name`	VARCHAR ( 255 ),
+	PRIMARY KEY(`id`)
+)
+");
+$db->query("
+CREATE TABLE `stock` (
+	`id`	INT,
+	`name`	VARCHAR ( 255 ),
+	`low`	DECIMAL ( 10 , 2 ),
+	`stock`	INTEGER,
+	`expansion_code`	varchar ( 10 ),
+	`avg`	DECIMAL ( 10 , 2 ),
+	`my_price`	DECIMAL ( 10 , 2 ),
+	`cheapest_in_country`	DECIMAL ( 10 , 2 ),
+	PRIMARY KEY(`id`)
+);
+");
